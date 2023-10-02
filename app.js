2f9d76d1d92aa536b3614b9da957516f5979a089
BHANU <21eg106b48@anurag.edu.in>
2023-10-02 18:28:51 +0530

    Api

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./app_server/routes/index');
-var usersRouter = require('./app_server/routes/users');
+// Import your route modules
+const routesApi = require('./app_api/routes/index');
+const indexRouter = require('./app_server/routes/index');
+const usersRouter = require('./app_server/routes/users');
 
-var app = express();
+const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'app_server/views'));
+// View engine setup
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
@@ -19,23 +21,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Define your routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/api', routesApi); // Mount the API routes under the '/api' path
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404)); 
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+// Error handler
+app.use(function (err, req, res, next) {
+  // Set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
 
 module.exports = app;
