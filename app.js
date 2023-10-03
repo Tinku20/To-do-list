129ea768cc06677b5660ee53e2b051f3cfc74b08
BHANU <21eg106b48@anurag.edu.in>
2023-10-03 12:52:47 +0530

    New change

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+require ('./app_api/models/db');
 
 // Import your route modules
 const routesApi = require('./app_api/routes/index');
--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,22 +1,77 @@
-var mongoose = require('mongoose'); 
-require('dotenv').config();
-const PORT = process.env.PORT || 3000;
-var gracefulShutdown;
-var dbURI = "mongodb://localhost:27017/To-Do-List"; 
-if(process.env.NODE_ENV==='production'){
-    dbURI = process.env.MONGODB_URI;
+const mongoose = require('mongoose');
+const PORT = process.env.PORT || 3000
+const readLine = require('readline');
+
+let dbURL = 'mongodb+srv://hello:wpm@cluster0.d7guwvr.mongodb.net/?retryWrites=true&w=majority';
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB Connected: ' + process.env.MONGO_URI);
+    } catch (error) {
+        console.log('Error: ' + error);
+        process.exit(1);
+    }
 }
-mongoose.connect(dbURI); 
-mongoose.connection.on('connected', function () { 
-    console.log('Mongoose connected to ' + dbURI); 
-});
-mongoose.connection.on('error', function (err) { 
-    console.log('Mongoose connection error: ' + err); 
-});
-mongoose.connection.on('disconnected', function () { 
-    console.log('Mongoose disconnected'); 
-}); 
 
-  
+const connect = () => {
+    setTimeout(() => mongoose.connect(dbURL), 1000);
+}
 
-require('./locations');
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose connected to ' + dbURL);
+});
+
+mongoose.connection.on('error', err => {
+    console.log('Error: ' + err);
+    return connect();
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Disconnected');
+});
+
+if (process.platform === 'win32') {
+    const rl = readLine.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on('SIGINT', () => {
+        process.emit("SIGINT");
+    });
+}
+
+async function gracefulShutdown(msg) {
+    const closed = await mongoose.connection.close()
+        .then(function () {
+            console.log(`Mongoose disconnected through ${msg}`);
+            process.exit();
+        });
+};
+
+process.once('SIGUSR2', () => {
+    gracefulShutdown('nodemon restart', () => {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+process.on('SIGINT', () => {
+    gracefulShutdown('app termination', () => {
+        process.exit(0);
+    });
+});
+process.on('SIGTERM', () => {
+    gracefulShutdown('Cyclic app shutdown', () => {
+        process.exit(0);
+    });
+});
+
+if (process.env.NODE_ENV === 'production') {
+    connectDB().then(() => {
+        app.listen(PORT, () => {
+            console.log("listening for requests");
+        })
+    })
+}
+else {
+    connect();
+}
